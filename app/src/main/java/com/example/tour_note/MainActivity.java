bd2a20f621f2215ca0361e2e634922d8d4766f22
johantrieu <johantrieu@gmail.com>
2020-06-09 06:50:34 +0300

    initial commit

---
+++ app/src/main/java/com/example/tour_note/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.tour_note;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.e(TAG,"This is an Error log");
+        Log.w(TAG,"This is a Warning log");
+        Log.i(TAG,"This is an Information log");
+        Log.d(TAG,"This is a Debug log");
+        Log.v(TAG, "This is a Verbose log");
+
+        //throw new RuntimeException("This is a crash");
+    }
+}
